fea601cb0520d47492e9b1938d247b5a67116623
Meet Patel <meetaiims@yahoo.com>
2023-03-12 16:54:54 -0400

    add SqlConnection class to perform pythonic operations on sql server

--- sql_con.py
+++ sql_con.py
@@ -1,80 +1,65 @@
-from config import SQL_DRIVER, SQL_SERVER, SQL_DB, SQL_USER, SQL_PWD
 import pyodbc
 
-def conn_string(driver, server, db, user, pwd):
-    """
-    Create and return sql server connection string
-    """
-    return f"Driver={driver};Server={server};Database={db};Authentication=SqlPassword;Encrypt=yes;TrustServerCertificate=Yes;UID={user};PWD={pwd}"
+class SQLConnection:
+    def __init__(self, driver, server, db, user, pwd):
+        self.driver = driver
+        self.server = server
+        self.db = db
+        self.user = user
+        self.pwd = pwd
 
-# create database connection instance
-def conn_odbc(driver=SQL_DRIVER, server=SQL_SERVER, db=SQL_DB, user=SQL_USER, pwd=SQL_PWD):
-    """
-    Create and return database connection instance
-    """
-    try:
-        conn = pyodbc.connect(conn_string(driver, server, db, user, pwd))
-        return conn
-    except pyodbc.Error as e:
-        print("Connection Error:")
-        print(e.args[1])
+    def get_conn_string(self):
+        return f"Driver={self.driver};Server={self.server};Database={self.db};Authentication=SqlPassword;Encrypt=yes;TrustServerCertificate=Yes;UID={self.user};PWD={self.pwd}"
+    
+    def get_conn_odbc(self):
+        try:
+            conn = pyodbc.connect(self.get_conn_string())
+            return conn
+        except pyodbc.Error as e:
+            print("Connection Error:")
+            print(e.args[1])
 
-# execute sql statement
-def select_records(cnxn, query):
-    """
-    Create cursor connection, execute sql query, and return all fetched records
-    """
-    try:
-        cursor = cnxn.cursor()
-        cursor.execute(query)
-        if cursor.rowcount != -1:
+    def select_records(self, query):
+        cnxn = self.get_conn_odbc()
+        try:
+            cursor = cnxn.cursor()
+            cursor.execute(query)
             rows = cursor.fetchall()
-            cursor.commit()
+            cnxn.commit()
             return rows
-        else:
-            cursor.commit()
-    except Exception as e:
-        cursor.rollback()
-        print(e.args[1])
-    finally:
-        cursor.close()
-        cnxn.close()
+        except Exception as e:
+            cnxn.rollback()
+            print(e.args[1])
+        finally:
+            cursor.close()
+            cnxn.close()
 
-def update_records(cnxn, query):
-    """
-    Create cursor connection, execute sql update query
-    """
-    try:
-        cursor = cnxn.cursor()
-        cursor.execute(query)
-        cursor.commit()
-    except Exception as e:
-        cursor.rollback()
-        print(e.args[1])
-    finally:
-        cursor.close()
-        cnxn.close()
+    def insert_records(self, query, params):
+        cnxn = self.get_conn_odbc()
+        try:
+            cursor = cnxn.cursor()
+            cursor.executemany(query, params)
+            cnxn.commit()
+        except Exception as e:
+            cnxn.rollback()
+            print(e.args[1])
+        finally:
+            cursor.close()
+            cnxn.close()
 
-# execute sql insert statement
-def insert_records(cnxn, query, params):
-    """
-    Create cursor connection, execute sql query, and return all fetched records
-    """
-    try:
-        cursor = cnxn.cursor()
-        cursor.executemany(query, params)
-        if cursor.rowcount != -1:
-            rows = cursor.fetchall()
-            cursor.commit()
-            return rows
-        else:
-            cursor.commit()
-    except Exception as e:
-        cursor.rollback()
-        print(e.args[1])
-    finally:
-        cursor.close()
-        cnxn.close()
+    def execute_ddl_dml(self, query):
+        cnxn = self.get_conn_odbc()
+        try:
+            cursor = cnxn.cursor()
+            cursor.execute(query)
+            cnxn.commit()
+        except Exception as e:
+            cnxn.rollback()
+            print(e.args[1])
+        finally:
+            cursor.close()
+            cnxn.close()
+
 
 # function to read file contents
 def read_contents(path):
