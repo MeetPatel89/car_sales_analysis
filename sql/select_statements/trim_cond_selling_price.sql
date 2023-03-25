ca34bf8d8d4c3ba0179acc2a783c3fc214346f66
Meet Patel <meetaiims@yahoo.com>
2023-03-25 16:16:35 -0400

    use condition_bins view to aggregate average selling price for filtered trims ('XLT', 'XL', 'Platinum', 'SVT Raptor') and condition bins

--- sql/select_statements/trim_cond_selling_price.sql
+++ sql/select_statements/trim_cond_selling_price.sql
@@ -1,23 +1,10 @@
 with filtered_sales as (
-select * from car_sales
+select trim, condition_bins, selling_price from condition_bins
 where trim in 
 ('XLT', 'XL', 'Platinum', 'SVT Raptor')
 and color != 'orange'),
-cond_binned_data as (
-select trim, selling_price,
-case
-	when condition between 1 and 1.5 then '1-1.5'
-	when condition > 1.5 and condition <= 2.0 then '1.5-2.0'
-	when condition > 2.0 and condition <= 2.5 then '2.0-2.5'
-	when condition > 2.5 and condition <= 3.0 then '2.5-3.0'
-	when condition > 3.0 and condition <= 3.5 then '3.0-3.5'
-	when condition > 3.5 and condition <= 4.0 then '3.5-4.0'
-	when condition > 4.0 and condition <= 4.5 then '4.0-4.5'
-	when condition > 4.5 and condition <= 5.0 then '4.5-5.0'
-end condition_bins
-from filtered_sales),
 pvt_tab as (
-select * from cond_binned_data
+select * from filtered_sales
 pivot
 (avg(selling_price) for condition_bins in ([1-1.5], [1.5-2.0], [2.0-2.5], [2.5-3.0], [3.0-3.5], [3.5-4.0], [4.0-4.5], [4.5-5.0])) as pvt_data)
 select * from pvt_tab
