7594d6bc91b925c7e71760793e4fbd42d37e335f
Meet Patel <meetaiims@yahoo.com>
2023-03-25 16:10:18 -0400

    use condition_bins view to aggregate average selling price for make and condition bins for top 10 makes(popular brands)

--- sql/select_statements/make_cond_price.sql
+++ sql/select_statements/make_cond_price.sql
@@ -3,22 +3,8 @@ with top10makes as (
 select top 10 make from car_sales
 group by make 
 order by count(*) desc),
-condition_bins_tab as (
-select make,
-case
-	when condition between 1 and 1.5 then '1-1.5'
-	when condition > 1.5 and condition <= 2.0 then '1.5-2.0'
-	when condition > 2.0 and condition <= 2.5 then '2.0-2.5'
-	when condition > 2.5 and condition <= 3.0 then '2.5-3.0'
-	when condition > 3.0 and condition <= 3.5 then '3.0-3.5'
-	when condition > 3.5 and condition <= 4.0 then '3.5-4.0'
-	when condition > 4.0 and condition <= 4.5 then '4.0-4.5'
-	when condition > 4.5 and condition <= 5.0 then '4.5-5.0'
-end condition_bins,
-selling_price
-from car_sales),
 top10_sales as (
-select make, condition_bins, selling_price from condition_bins_tab a
+select make, condition_bins, selling_price from condition_bins a
 where exists (
 select * from top10makes b
 where a.make = b.make))
